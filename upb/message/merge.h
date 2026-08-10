6be3d434af13f4de3a3a8aec4f6a95413e31d4c9
Ada Zhang <adazhang@google.com>
2026-08-09 21:31:11 -0700

    Implement a native, recursive in-memory message merge for upb.
    PiperOrigin-RevId: 961926767

--- upb/message/merge.h
+++ upb/message/merge.h
@@ -3,7 +3,6 @@
 
 #include "upb/mem/arena.h"
 #include "upb/message/message.h"
-#include "upb/mini_table/extension_registry.h"
 #include "upb/mini_table/message.h"
 
 // Must be last.
@@ -13,9 +12,35 @@
 extern "C" {
 #endif
 
-UPB_NODISCARD UPB_API bool upb_Message_MergeFrom(
-    upb_Message* dst, const upb_Message* src, const upb_MiniTable* mt,
-    const upb_ExtensionRegistry* extreg, upb_Arena* arena);
+// Merges the contents of the source message `src` into the destination
+// message `dst`.
+//
+// In detail:
+// - Normal fields and canonical extensions are merged according to the rules of
+// proto merging.
+// - Unknown fields and non-canonical extensions are copied directly to the
+// destination without being parsed or promoted as normal fields or canonical
+// extensions.
+//
+// Both `dst` and `src` must conform to the schema and layout defined by `mt`.
+// If the messages are defined by different (but compatible) minitables,
+// encode and then decode, or use `upb_Message_Convert()` to first convert the
+// source message to the destination minitable if applicable.
+//
+// Self-merge is not supported and will return false. This check guards
+// against self-merging at any of the following levels:
+// - The root messages (`dst` and `src` pointing to the same message)
+// - Submessage fields recursively at any depth
+// - Repeated fields (arrays)
+// - Map fields
+//
+// Note: This function always performs a deep copy of submessages, strings,
+// bytes, maps, and repeated fields (arrays). Newly copied values and structures
+// are allocated on the destination message's arena.
+UPB_NODISCARD UPB_API bool upb_Message_MergeFrom(upb_Message* dst,
+                                                 const upb_Message* src,
+                                                 const upb_MiniTable* mt,
+                                                 upb_Arena* arena);
 
 #ifdef __cplusplus
 } /* extern "C" */
